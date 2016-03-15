7f53f3cec8d0624e68763f13281beb7c9a0daa34
Nathan Johnson <nathancj224@gmail.com>
2016-03-15 00:46:00 -0400

    LowerIntakeInAuto for all auto commands

--- src/org/usfirst/frc/team5517/robot/commands/AutoRoughTerrain.java
+++ src/org/usfirst/frc/team5517/robot/commands/AutoRoughTerrain.java
@@ -24,7 +24,7 @@ public class AutoRoughTerrain extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-        addSequential(new LowerIntake());
+        addSequential(new LowerIntakeInAuto());
         addSequential(new ManualDrive(0.6, 0, 2.5));
     }
 }
